446d39bd0d00080f87c86f4f84c4dffa527ed9a1
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-20 22:02:29 -0700

    Update tt_um_uabc_test2027.v

--- src/tt_um_uabc_test2027.v
+++ src/tt_um_uabc_test2027.v
@@ -37,6 +37,10 @@ module tt_um_uabc_test2027 (
     end
 
     seg7 seg7(.digit(display_value), .segments(segment_reg));
+
+  assign uo_out = segment_reg; 
+  assign uio_out = 0; 
+  assign uio_oe = 8'hFF; 
     
   assign uo_out = segment_reg; 
 
